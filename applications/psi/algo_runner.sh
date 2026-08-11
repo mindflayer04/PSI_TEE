c5ee6b3ea48501f466e7d35e1c6f1835d1e1ea5c
Mn031 <nayakmayash@gmail.com>
2026-08-12 02:09:32 +0530

    Fix CORE_ID to 1 in algo_runner.sh for correct CPU core assignment

--- applications/psi/algo_runner.sh
+++ applications/psi/algo_runner.sh
@@ -8,7 +8,7 @@ SGX_MODE=HW # HW or SIM
 # Algorithms:
 MIN_ENCLAVE_SIZE=4096    # 4 GB
 MAX_ENCLAVE_SIZE=4096
-CORE_ID=5 # the cpu core id to run the program
+CORE_ID=1 # the cpu core id to run the program
 DISK_IO=0 # 0: no disk IO, 1: disk IO
 TCS_NUM=2
 
