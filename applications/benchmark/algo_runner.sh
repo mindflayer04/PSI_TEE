70b7be93a7e25ac294323e7ced094dda4cfc1fff
mindflayer04 <aashirwadmishra03@gmail.com>
2026-08-20 06:56:01 +0000

    minor changes

--- applications/benchmark/algo_runner.sh
+++ applications/benchmark/algo_runner.sh
@@ -6,8 +6,8 @@ cp ../../omap/common/encutils.cpp ./Enclave/TrustedLibrary/encutils.cpp
 SGX_MODE=HW # HW or SIM
 
 # Algorithms:
-MIN_ENCLAVE_SIZE=194560   # 190 GB
-MAX_ENCLAVE_SIZE=194560   # 190 GB
+MIN_ENCLAVE_SIZE=256   
+MAX_ENCLAVE_SIZE=1024   
 CORE_ID=1 # the cpu core id to run the program
 DISK_IO=1 # 0: no disk IO, 1: disk IO
 TCS_NUM=32
